00f136d5a4eea3e3a0a11c8a39e69a4ae6166a58
Leonardo <leonardo.fg95@gmail.com>
2022-05-03 21:25:08 -0300

    Ajustar tamanho do palco do jogo

---
+++ scripts/game.js
@@ -0,0 +1,13 @@
+// Ajustar tamanho do palco do jogo
+
+var altura = 0
+var largura = 0
+
+function ajustarTamanhoPalcoJogo() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(altura, largura)
+}
+
+ajustarTamanhoPalcoJogo()
