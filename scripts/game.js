1c43667126bf5147057e38e8221bcc720934f9ca
Leonardo <leonardo.fg95@gmail.com>
2022-05-13 21:00:06 -0300

    Adicionando o cronômetro

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 10
 
 function ajustarTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -12,6 +13,22 @@ function ajustarTamanhoPalcoJogo() {
 }
 
 ajustarTamanhoPalcoJogo()
+
+// Cronômetro
+
+var cronometro = setInterval(function() {
+    
+    tempo -= 1
+    
+    if (tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        alert('Vitória')
+    } else {
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+    
+}, 1000)
 
 // Posições randomicas
 
