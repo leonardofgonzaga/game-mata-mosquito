80ea49071e69e44b608f38edf458c86c503ccf62
Leonardo <leonardo.fg95@gmail.com>
2022-05-13 21:37:02 -0300

    Criando a página inicial

--- scripts/game.js
+++ scripts/game.js
@@ -99,5 +99,16 @@ function ladoAleatorio() {
     }
 }
 
+// Iniciar Jogo 
 
+function iniciarJogo() {
+    var nivel = document.getElementById('nivel').value
+
+    if (nivel === '') {
+        alert('Selecione um nível para iniciar o jogo')
+        return false
+    }
+
+    alert(nivel)
+}
  
